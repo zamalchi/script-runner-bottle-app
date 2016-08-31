a75486cb4235798614be5380a9b4338bcf3bf74d
Eric <zamalchi@techsquare.com>
2016-08-31 12:42:23 -0400

    scss transpiler script : adds variables

--- src/scss/transpile.sh
+++ src/scss/transpile.sh
@@ -2,6 +2,9 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
+scss_file="base.scss"
+css_file="main.css"
+
 cd $parent_path
 
-sass --watch ./base.scss:../../static/css/main.css
+sass --watch ./${scss_file}:../../static/css/${css_file}
