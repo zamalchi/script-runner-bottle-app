063b30955ea5d5a502aaeda45a96657e730f3216
zamalchi <zamalchi@techsquare.com>
2016-11-21 17:31:23 -0500

    trying to fix classes/Slurm.py

--- classes/Slurm.py
+++ classes/Slurm.py
@@ -20,9 +20,8 @@ class Slurm:
     ### STATIC METHODS START
     ####################################################################################################
 
-    ###### SINFO METHODS
+    ###### HELPER METHODS
 
-    # used by : Slurm.parseNodeNames
     @staticmethod
     def normalizeNodeName(nodeName):
         # receives a single-node name (ex. 32)
@@ -36,7 +35,6 @@ class Slurm:
 
         return nodeName.strip().zfill(3)
 
-    # used by : Slurm.parseStateOutputToList
     @staticmethod
     def parseNodeNames(rawNodeNames):
         # recieves an unparsed string of nodes (ex. "node[018-021, 34, 45-67]")
@@ -65,73 +63,7 @@ class Slurm:
 
         return sorted(parsedNodeNames)
 
-    # used by : Slurm.getSingleStateOutput
-    @staticmethod
-    def parseStateOutputToList(raw):
-        # param raw <str> : raw output from sinfo command
-        # return <[ ([str], str, str) | str ]> : a parsed list of output tuples for one state
-        # [ ( [nodenames], time, reasons ), ... ]
-        # list may also contain a string element, if the line parse was unsuccessful
-
-        parsedList = []
-
-        # for every line in this state
-        for line in raw:
-
-            # if there are 3 fields available to parse, continue...
-            if len(line.split("\t")) == 3:
-                nodes, time, reasons = line.split("\t")
-
-            elif type(line) is list and len(line) == 3:
-
-                nodes, time, reasons = line
-
-                nodes = Slurm.parseNodeNames(nodes)
-
-                parsedList.append((nodes, time, reasons))
-
-            # if there are not 3 fields to parse, append the original line
-            else:
-                parsedList.append(line)
-
-        return parsedList
-
-    ### ^ PRIVATE | PUBLIC v
-
-    # used by : Slurm.__init__
-    @staticmethod
-    def getSingleStateOutput(state):
-        # FOR PUBLIC USE
-        # param state <str> : state name
-        # return <[ ([str], str, str) | str ]> : lines of output for state
-        # return is passed directly from Slurm.parseStateOutputToList
-
-        from commands import getstatusoutput
-
-        # %E : reasons for state
-        # %H : timestamp
-        # %N : node list
-        output_format = '"%N\t%20H\t%E"'
-
-        # -a : all
-        # -h : no header
-        # -N : node format
-        # -R : list reasons
-        # -t : states
-        # -o : output format
-        cmd = "sinfo -a -h -N -o {0} -R -t {1}".format(output_format, state)
-
-        # getstatusoutput attaches [0] field (return flag?)
-        # for each state, get a list of output lines (each line conforms to output_format)
-        output = getstatusoutput(cmd)[1].split("\n")
-
-        # modify each line in this state
-        # if the line has a long nodelist (length > 35),
-        #   it will display the nodelist on a separate line with the other information aligned on the next line
-        # if the line has a short nodelist (length <= 35), it will not modify the line
-        # output = getModifiedOutput(output)
-
-        return Slurm.parseStateOutputToList(output)
+    ###### SINFO METHODS
 
     @staticmethod
     def getNonEmptyStates():
@@ -166,7 +98,7 @@ class Slurm:
         return getstatusoutput(cmd)[1]
 
     @staticmethod
-    def getScontrolShowReservation():
+    def getScontrolShowReservations():
         # FOR PUBLIC USE
         # return <list[Reservation]> : output from scontrol
 
@@ -174,14 +106,9 @@ class Slurm:
 
         cmd = "scontrol -o show reservation"
 
-        output = getstatusoutput(cmd)[1]
+        output = getstatusoutput(cmd)[1].split("\n")
 
-        reservations = []
-
-        for each in output.split("\n"):
-            reservations.append(Slurm.Reservation(each))
-
-        return reservations
+        return [Slurm.Reservation(each) for each in output]
 
     ####################################################################################################
     ### STATIC METHODS END
@@ -204,25 +131,29 @@ class Slurm:
             return self.__entries
 
         # used by : Slurm.getNonEmptyStates
-        def __init__(self, state, output=None):
-            if state.__class__.__name__ == "State":
-                self = state
-            else:
-                if output is not None:
-                    raw = output.split("\n")
-                else:
-                    raw = Slurm.getSingleStateOutput(state)
+        def __init__(self, state):
 
-                entries = []
+            from commands import getstatusoutput
 
-                for each in raw:
-                    entry = Slurm.Entry(each)
+            # %E : reasons for state
+            # %H : timestamp
+            # %N : node list
+            output_format = '"%N\t%20H\t%E"'
 
-                    if entry.nodes:
-                        entries.append(entry)
+            # -a : all
+            # -h : no header
+            # -N : node format
+            # -R : list reasons
+            # -t : states
+            # -o : output format
+            cmd = "sinfo -a -h -N -o {} -R -t {}".format(output_format, state)
 
-                self.__name = state
-                self.__entries = entries
+            # getstatusoutput attaches [0] field (return flag?)
+            # for each state, get a list of output lines (each line conforms to output_format)
+            output = getstatusoutput(cmd)[1].split("\n")
+
+            self.__name = state
+            self.__entries = [Slurm.Entry(each) for each in output]
 
         def findNodeInEntries(self, node):
             # return <int> : index (in self.__entries) where the node is located (or -1 if not found)
@@ -260,9 +191,12 @@ class Slurm:
 
         def __init__(self, entry):
             if type(entry) is str:
-                entry = entry.split("\t")
+                nodes, time, reason = entry.split("\t")
+                self.__nodes = Slurm.parseNodeNames(nodes)
+                self.__time = time
+                self.__reason = reason
 
-            if type(entry) is list and len(entry) == 3:
+            elif type(entry) is list and len(entry) == 3:
                 self.__nodes, self.__time, self.__reason = entry
             else:
                 self.__nodes = []
@@ -282,38 +216,39 @@ class Slurm:
         @property
         def nodes(self):
             return self.__nodes
-        
-        @property
-        def data(self):
-            return self.__data
 
         @property
         def state(self):
             return self.__state
 
+        @property
+        def data(self):
+            return self.__data
+
+        
         def __init__(self, raw):
-            if raw.__class__.__name__ == "Reservation":
-                self = raw
-            else:
-                fields = filter(None, raw.split(' '))
-                data = {}
+            # if raw.__class__.__name__ == "Reservation":
+            #     self = raw
+            # else:
+            fields = filter(None, raw.split(' '))
+            data = {}
 
-                for f in fields:
-                    key, val = f.strip().split('=')
+            for f in fields:
+                key, val = f.strip().split('=')
 
-                    if key == "ReservationName":
-                        self.__name = val
+                if key == "ReservationName":
+                    self.__name = val
 
-                    elif key == "Nodes":
-                        self.__nodes = Slurm.parseNodeNames(val)
+                elif key == "Nodes":
+                    self.__nodes = Slurm.parseNodeNames(val)
 
-                    elif key == "State":
-                        self.__state = val
+                elif key == "State":
+                    self.__state = val
 
-                    else:
-                        data[key] = val
+                else:
+                    data[key] = val
 
-                self.__data = data
+            self.__data = data
     
     
     ####################################################################################################
