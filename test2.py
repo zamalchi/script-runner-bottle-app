af4c1f475a5db1ba43d4caf3529773f5a23f1260
zamalchi <zamalchi@techsquare.com>
2016-11-22 16:10:56 -0500

    adding node tests

--- test2.py
+++ test2.py
@@ -16,7 +16,6 @@ def formatStr(raw):
 
 class TestSlurmSuite(unittest.TestCase):
 
-
     def test_state(self):
         states = Slurm.getNonEmptyStates()
 
@@ -28,7 +27,7 @@ class TestSlurmSuite(unittest.TestCase):
         print("OK : States are all Slurm.State objects")
 
         for s in states.values():
-            self.assertTrue(s.entries)
+            self.assertTrue(s.hasEntries())
         print("OK : All states have entries")
 
         for s in states.values():
@@ -36,32 +35,57 @@ class TestSlurmSuite(unittest.TestCase):
                 self.assertTrue(e.__class__.__name__ == "Entry")
         print("OK : All entries are Slurm.Entry objects")
 
+        for s in states.values():
+            for e in s.entries:
+                self.assertTrue(e.nodes and e.time and e.reason)
+        print("OK : All entries have nodes, time, and reason attributes")
+
+    def test_reservation(self):
+        reservations = Slurm.getReservations()
+
+        self.assertTrue(reservations)
+        print("OK : List of reservations exists")
+
+        for r in reservations:
+            self.assertTrue(r.name and r.nodes and r.state and r.data)
+        print("OK : All reservations have name, nodes, state, and data attributes")
+
+        for r in reservations:
+            for n in r.nodes:
+                self.assertTrue(n == Slurm.normalizeNodeName(n))
+        print("OK : All reserved nodes are in normalized format")
+
+        for r in reservations:
+            self.assertTrue(type(r.data) is dict)
+        print("OK : All reservations' data attribute are type dictionary")
+
+    def test_node(self):
+        nodes = []
+        nodes.append(Slurm.Node("034"))
+        nodes.append(Slurm.Node(100))
+        nodes.append(Slurm.Node("node123"))
+
+        for n in nodes:
+            self.assertTrue(n.__class__.__name__ == "Node")
+        print("OK : All nodes are Slurm.Node objects")
+
+        for n in nodes:
+            self.assertTrue(n.found)
+        print("OK : All nodes found")
+
+        for n in nodes:
+            self.assertTrue(n.state and n.data)
+        print("OK : All nodes have state and data attributes")
+
+        for n in nodes:
+            self.assertTrue(type(n.data) is dict)
+        print("OK : All nodes' data attribute are type dictionary")
+
+        for n in nodes:
+            self.assertTrue(n.name == Slurm.normalizeNodeName(n.name))
+        print("OK : All nodes' names are in normalized format")
 
 
-    # def test_reservation(self):
-    #     print
-    #     i = 0
-    #     if self.liveData:
-    #         rawOutput["reservations"] = Slurm.getScontrolShowReservation()
-    #         print(formatStr("Reservations:") + "(LIVE)")
-    #
-    #     else:
-    #         print(formatStr("Reservations:") + "(MOCK)")
-    #
-    #     for each in rawOutput["reservations"]:
-    #         obj = Slurm.Reservation(each)
-    #
-    #         self.assertTrue(obj.__class__.__name__ == "Reservation")
-    #
-    #         if not self.liveData:
-    #             self.assertTrue(obj.name == tests["reservations"]["name"][i])
-    #             self.assertTrue(len(obj.nodes) == tests["reservations"]["nodeCount"][i])
-    #             self.assertTrue(obj.state == tests["reservations"]["state"][i])
-    #             self.assertTrue(type(obj.data) is dict)
-    #
-    #         i += 1
-    #
-    #     print(formatStr("Reservations:") + "OK")
 
 if __name__ == '__main__':
 
