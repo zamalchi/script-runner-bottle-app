1da919a4a217b2e047c3539fcc9e485bdcf6db00
zamalchi <zamalchi@techsquare.com>
2016-12-07 14:34:17 -0500

    ...

--- modu/tests/context.py
+++ modu/tests/context.py
@@ -12,7 +12,9 @@ from __future__ import absolute_import
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 ################################################
 
